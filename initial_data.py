31613971a8c950aa27bc5f913e0b897147e1241e
aman-py-8 <21f1005863@ds.study.iitm.ac.in>
2024-10-29 23:55:31 +0530

    fixed a bug with initial data creation

--- initial_data.py
+++ initial_data.py
@@ -2,6 +2,7 @@ from main import app
 from application.models import db
 from application.secondry import datastore
 from werkzeug.security import generate_password_hash
+import uuid
 
 with app.app_context():
     db.create_all()
@@ -20,12 +21,20 @@ with app.app_context():
     # Create users and assign roles
     if not datastore.find_user(email="admin@email.com"):
         admin = datastore.create_user(
-            name = "admin", email="admin@email.com", password=generate_password_hash("admin"))
+            name="admin",
+            email="admin@email.com",
+            password=generate_password_hash("admin"),
+            fs_uniquifier=str(uuid.uuid4())  # Assign a unique identifier
+        )
         datastore.add_role_to_user(admin, "admin")
-        
+
     if not datastore.find_user(email="inst1@email.com"):
         instructor = datastore.create_user(
-            name = "instructor", email="inst1@email.com", password=generate_password_hash("inst1"))
+            name="instructor",
+            email="inst1@email.com",
+            password=generate_password_hash("instructor"),
+            fs_uniquifier=str(uuid.uuid4())
+        )
         datastore.add_role_to_user(instructor, "instructor")
-    
+
     db.session.commit()
